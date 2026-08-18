bd1210f3ce58d27f2248bac84470fdee568164ee
hxczzza <3680968503@qq.com>
2026-08-18 23:48:46 +0800

    unfinished-1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,16 @@
 board=[[0]*6 for _ in range(6)]
+
 while True:
+    try:
+        boardsize = int(input("Enter the board size (0-6): "))
+        if 0 <= boardsize <= 6:
+            break
+        else:
+            print("Row and column must be between 0 and 6.")
+    except ValueError:
+        print("Invalid input. Please try again.")
+    except KeyboardInterrupt:
+        print("\nExiting the program.")
+        break
+
 
